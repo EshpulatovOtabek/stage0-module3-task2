b34b247ba7848e0727a87f125d02b267eab2c8d7
Otabek Eshpulatov <105120956+EshpulatovOtabek@users.noreply.github.com>
2022-08-14 15:50:11 +0500

    Update NamingConvention.java

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -1,15 +1,13 @@
-package lang.print.gaps.task2;
-
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
-        int variableOne = 1;
-        int variableTwo = 2;
-        int variableThree = 3;
-        final int VARIABLE_FOUR = 4;
-        final int VARIABLE_fIVE = 5;
-        int VariableSix = 6;
-        int variableseven = 7;
-        int VariableEight = 8;
+         int _variable = 10;
+         int variable_One = 1;
+         int variableTwo = 2;
+         int variable_three = 3;
+         final int VARIABLE_FOUR = 4;
+         final int VARIABLE_FIVE = 5;
+         int VariableSix = 6;
+         int variableseven = 7;
+         int VariableEight = 8;
     }
 }
